05fa7f021ab8e7fabc337f729ec725a17b7f742f
Seungjae Park <astro.psj@gmail.com>
2017-11-25 22:17:42 +0900

    Add teacher's schedule viewer

--- renderer.js
+++ renderer.js
@@ -11,7 +11,27 @@ const updateScheduleViewer = async (which) => {
   $viewer.empty()
   if (schoolName) {
     if (which === 'for-teacher') {
-      Materialize.toast('현재 지원하지 않는 기능입니다.', 3000)
+      let teacherData = $('select#teacher').val()
+      if (teacherData) {
+        const schedules = await school.getTeacherSchedules(school.find('', schoolName), teacherData)
+        if (schedules !== null) {
+          for (let i = 0; i < 8; i += 1) {
+            let buf = '<tr>'
+            for (let j = 1; j < 6; j += 1) {
+              const value = schedules[j][i]
+              buf += `<td class="${value && value.isChanged ? 'blue-text' : ''}${value ? ' tooltipped" data-tooltip="' + value.subjectOriginal : ''}">${value ? `${value.subject}<br>(${value.grade}학년 ${value.room}반)` : '없음'}</td>`
+            }
+            buf += '</tr>'
+            $viewer.append(buf)
+          }
+          $('.tooltipped').tooltip({delay: 50})
+          Materialize.toast('시간표가 업데이트 되었습니다.', 3000)
+        } else {
+          Materialize.toast('시간표를 불러올 수 없습니다. 인터넷 상태를 확인해주세요.', 3000)
+        }
+      } else {
+        Materialize.toast('교사 정보를 입력해주세요.', 3000)
+      }
     } else if (which === 'for-student') {
       let studentData = $('select#student').val()
       if (studentData) {
@@ -61,8 +81,19 @@ const updateSchoolData = async (name) => {
       $studentSelector.append('<option value="3|' + i + '">3학년 ' + i + '반</option>')
     }
     $studentSelector.material_select()
+    $('#for-student .select-wrapper ul.select-dropdown li').click(() => {
+      updateScheduleViewer('for-student')
+    })
     $teacherSelector.empty()
+    const teacherNames = comciganData.성명.filter(value => value).sort()
+    $teacherSelector.append('<option value="" disabled selected>교사명</option>')
+    for (let i = 0, len = teacherNames.length; i < len; i += 1) {
+      $teacherSelector.append('<option value="' + teacherNames[i] + '">' + teacherNames[i] + '</option>')
+    }
     $teacherSelector.material_select()
+    $('#for-teacher .select-wrapper ul.select-dropdown li').click(() => {
+      updateScheduleViewer('for-teacher')
+    })
   } else {
     Materialize.toast('컴시간 알리미를 사용하지 않는 학교입니다.', 3000)
   }
